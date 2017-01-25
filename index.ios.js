f1a553529e8f5a16d59fbcf621453185abd86c80
Swizec Teller <swizec@swizec.com>
2017-01-25 01:15:04 -0800

    Define images

--- index.ios.js
+++ index.ios.js
@@ -9,8 +9,42 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableHighlight
 } from 'react-native';
+
+const Images = [
+    {
+        uri: "https://i.imgur.com/mxgtWKt.jpg",
+        label: "Cat on a blue blanket"
+    },
+
+    {
+        uri: "https://i.imgur.com/XCRnNWn.jpg",
+        label: "A cat toy"
+    },
+
+    {
+        uri: "https://i.imgur.com/dqQX1K0.jpg",
+        label: "A close up of a dog"
+    },
+
+    {
+        uri: "https://i.imgur.com/nZXbSbh.jpg",
+        label: "Sheep next to a cat"
+    },
+
+    {
+        uri: "https://i.imgur.com/mXCjefR.jpg",
+        label: "Cat laying on the grass"
+    },
+
+    {
+        uri: "https://i.imgur.com/AGyxRcc.jpg",
+        label: "Bird sitting on a railing"
+    }
+];
 
 export default class JustATributeApp extends Component {
   render() {
